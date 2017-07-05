230c806900d65f5f5d59539cd0461b3e9ee013ba
Oliver Dziedzic <o.dziedzic92@googlemail.com>
2017-07-05 14:50:50 +0200

    Insert Remember Login Checkbox

--- app/src/main/java/com/example/oli/scaleuser2/LoginGui.java
+++ app/src/main/java/com/example/oli/scaleuser2/LoginGui.java
@@ -2,10 +2,14 @@ package com.example.oli.scaleuser2;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,10 +26,20 @@ import android.widget.Toast;
  */
 
 
-public class LoginGui extends AppCompatActivity {
+public class LoginGui extends AppCompatActivity implements TextWatcher,
+        CompoundButton.OnCheckedChangeListener {
     public EditText user_name, passwd;
     public static String login_username;
     public static ProgressDialog loading;
+
+    private CheckBox rem_userpass;
+    private static final String PREF_NAME = "prefs";
+    private static final String KEY_REMEMBER = "remember";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASS = "password";
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     int progressValue;
     AlertDialog alertDialog;
 
@@ -33,9 +47,62 @@ public class LoginGui extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
         user_name = (EditText) findViewById(R.id.username);
         passwd = (EditText) findViewById(R.id.password);
+        rem_userpass = (CheckBox)findViewById(R.id.cb_remember);
+
+        if(sharedPreferences.getBoolean(KEY_REMEMBER, false))
+            rem_userpass.setChecked(true);
+        else
+            rem_userpass.setChecked(false);
+
+        user_name.setText(sharedPreferences.getString(KEY_USERNAME,""));
+        passwd.setText(sharedPreferences.getString(KEY_PASS,""));
+
+        user_name.addTextChangedListener(this);
+        passwd.addTextChangedListener(this);
+        rem_userpass.setOnCheckedChangeListener(this);
+
+
+
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        managePrefs();
+    }
+
+
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        managePrefs();
+    }
+
+    private void managePrefs(){
+        if(rem_userpass.isChecked()){
+            editor.putString(KEY_USERNAME, user_name.getText().toString().trim());
+            editor.putString(KEY_PASS, passwd.getText().toString().trim());
+            editor.putBoolean(KEY_REMEMBER, true);
+            editor.apply();
+        }else{
+            editor.putBoolean(KEY_REMEMBER, false);
+            editor.remove(KEY_PASS);//editor.putString(KEY_PASS,"");
+            editor.remove(KEY_USERNAME);//editor.putString(KEY_USERNAME, "");
+            editor.apply();
+        }
     }
 
     public void onLogin(View v){
